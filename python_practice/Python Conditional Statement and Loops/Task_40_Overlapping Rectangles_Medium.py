afcba74da0deca01bd0571821ad11751e77d28b9
mr-ahtashamulhaq <mr.ahtashamulhaq@gmail.com>
2025-07-30 23:30:37 +0500

    update

--- python_practice/Python Conditional Statement and Loops/Task_40_Overlapping Rectangles_Medium.py
+++ python_practice/Python Conditional Statement and Loops/Task_40_Overlapping Rectangles_Medium.py
@@ -1,9 +1,18 @@
-"""Given an integer n, write a program to return the factorial of n. The Factorial of a number is the product of all the numbers from 1 to n."""
+"""Given two rectangles, find if the given two rectangles overlap or not. A rectangle is denoted by providing the x and y coordinates of two points: the left top corner and the right bottom corner of the rectangle. Two rectangles sharing a side are considered overlapping. (L1 and R1 are the extreme points of the first rectangle and L2 and R2 are the extreme points of the second rectangle)."""
 #User function Template for python3
-def factorial(n):
-    fact=1
-    for i in range(1,n+1):
-        fact*=i
-    return fact
+class Solution:
+    
+    def doOverlap(self, L1, R1, L2, R2):
+        if R1[0]<L2[0] or R2[0]<L1[0] or L2[1]<R1[1] or L1[1]<R2[1]:
+            return 0
+        else:
+            return 1
+ 
+                    
 
-print(factorial(5))
+obj = Solution()
+L1  = [5,15]
+R1  = [15,5]
+L2  = [5,10]
+R2  = [10,5]
+print(obj.doOverlap(L1,R1,L2,R2))
